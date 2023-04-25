e9269ddbe3c20bcbb1f24d2cc5ffeb7287f1caf5
Wesllycode <wesllycode07@gmail.com>
2023-04-25 07:02:15 -0300

    feat: add onDelete

--- database/migrations/2023_04_25_000000_create_users_table.php
+++ database/migrations/2023_04_25_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignUuid('tenant_id')->references('id')->on('tenants');
+            $table->foreignUuid('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
             $table->string('name');          
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
