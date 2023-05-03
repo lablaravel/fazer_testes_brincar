7c019e4aeb7f9fe9ad7b39d07a0303352e20fa9b
Wesllycode <wesllycode07@gmail.com>
2023-05-03 03:46:39 -0300

    feat: tentei configurar para funcionar hot load do vite

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/js/app.js'
             ],
-            refresh: true,
+            refresh: [
+                'resources/routes/**',
+                'routes/**',
+                'resources/views/**',
+            ],
         }),
     ],
 });
